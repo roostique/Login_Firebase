493faa0bdfe8bfeb1a1e21684b2c0caa2b16944a
rustemsupayev <rustik7252@gmail.com>
2019-12-01 16:49:02 +0600

    Initial commit. Created UI, and added Sign-In/Sign-Out functionalities.

---
+++ LoginFirebase/SignUpController.swift
@@ -0,0 +1,158 @@
+//
+//  SignUpController.swift
+//  LoginFirebase
+//
+//  Created by Rustem Supayev on 29/11/2019.
+//  Copyright © 2019 Rustem Supayev. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignUpController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "3489-70xdhFoY")
+        return iv
+    }()
+    
+    lazy var emailContainerView: UIView = {
+        let view = UIView()
+        return view.textContainerView(view: view, #imageLiteral(resourceName: "3489-70xdhFoY"), emailTextField)
+    }()
+    
+    lazy var usernameContainerView: UIView = {
+        let view = UIView()
+        return view.textContainerView(view: view, #imageLiteral(resourceName: "3489-70xdhFoY"), usernameTextField)
+    }()
+    
+    lazy var passwordContainerView: UIView = {
+        let view = UIView()
+        return view.textContainerView(view: view, #imageLiteral(resourceName: "3489-70xdhFoY"), passwordTextField)
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let tf = UITextField()
+        return tf.textField(withPlaceolder: "Email", isSecureTextEntry: false)
+    }()
+    
+    lazy var usernameTextField: UITextField = {
+        let tf = UITextField()
+        return tf.textField(withPlaceolder: "Username", isSecureTextEntry: false)
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let tf = UITextField()
+        return tf.textField(withPlaceolder: "Password", isSecureTextEntry: true)
+    }()
+    
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("SIGN UP", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(UIColor.mainRed(), for: .normal)
+        button.backgroundColor = .white
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Already have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.white])
+        attributedTitle.append(NSAttributedString(string: "Sign In", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.white]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewComponents()
+        
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleSignUp() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let username = usernameTextField.text else { return }
+        
+        createUser(withEmail: email, password: password, username: username)
+    }
+    
+    @objc func handleShowLogin() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - API
+    
+    func createUser(withEmail email: String, password: String, username: String) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            
+            if let error = error {
+                print("Failed to create user with error: ", error.localizedDescription)
+                return
+            }
+            
+            guard let uid = result?.user.uid else { return }
+            
+            let values = ["email": email, "username": username]
+            
+            Database.database().reference().child("users").child(uid).updateChildValues(values, withCompletionBlock: { (error, ref) in
+                
+                if let error = error {
+                    print("Failed to update database with error: ", error.localizedDescription)
+                    return
+                }
+                
+                guard let navController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController else { return }
+                guard let controller = navController.viewControllers[0] as? HomeController else { return }
+                controller.configureViewComponents()
+                
+                controller.loadUserData()
+                self.dismiss(animated: true, completion: nil)
+            })
+        }
+        
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureViewComponents() {
+        view.backgroundColor = UIColor.mainRed()
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 60, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 150, height: 150)
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(emailContainerView)
+        emailContainerView.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 24, paddingLeft: 32, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
+        
+        view.addSubview(usernameContainerView)
+        usernameContainerView.anchor(top: emailContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 24, paddingLeft: 32, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
+        
+        view.addSubview(passwordContainerView)
+        passwordContainerView.anchor(top: usernameContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 16, paddingLeft: 32, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
+        
+        view.addSubview(loginButton)
+        loginButton.anchor(top: passwordContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 24, paddingLeft: 32, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 32, paddingBottom: 12, paddingRight: 32, width: 0, height: 50)
+        
+    }
+    
+    
+}
